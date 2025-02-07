0bef4a1e769f75e99c2bf974a56dfbb3499b9c94
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-07 23:52:57 +0600

    database connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -7,6 +8,20 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+
+//Database connection
+const uri = `mongodb+srv://${user}:${pass}@cluster0.pr0dk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(uri)
+.then(() => {
+    console.log(`Database connected`);
+})
+.catch(err => {
+    console.log(`error from db connection ${err}`);
+})
 
 
 app.get('/', (req, res) => {
