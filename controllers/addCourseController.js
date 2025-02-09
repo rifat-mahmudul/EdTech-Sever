835bff16fbad21e62180c4b09ffb33c545f96d4a
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-09 21:20:12 +0600

    get single course by id

--- controllers/addCourseController.js
+++ controllers/addCourseController.js
@@ -16,13 +16,25 @@ const getCourses = async(req, res) => {
         const result = await Courses.find();
         res.status(200).send(result);
     } catch (error) {
-        console.log(`An error from add course ${error}`);
-        res.status(500).send('An error from add course ${error}')
+        console.log(`An error from get course ${error}`);
+        res.status(500).send(`An error from get course ${error}`)
+    }
+}
+
+const getSingleCourse = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await Courses.findById(id);
+        res.status(200).send(result);
+    } catch (error) {
+        console.log(`An error from get single course ${error}`);
+        res.status(500).send(`An error from single course ${error}`)
     }
 }
 
 
 module.exports = {
     postCourse,
-    getCourses
+    getCourses,
+    getSingleCourse
 }
--- routes/addCourseHandler.js
+++ routes/addCourseHandler.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/verifyToken');
 const verifyAdmin = require('../middleware/verifyAdmin');
-const { postCourse, getCourses } = require('../controllers/addCourseController');
+const { postCourse, getCourses, getSingleCourse } = require('../controllers/addCourseController');
 
 router
     .route('/')
     .post(verifyToken, verifyAdmin, postCourse)
     .get(getCourses)
+
+router
+    .route('/:id')
+    .get(getSingleCourse)
 
 module.exports = router;
